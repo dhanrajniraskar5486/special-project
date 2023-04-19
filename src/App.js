3598bffa7e81eb1968c6557fb42e861a01461e91
Dhanraj Niraskar <dhanrajniraskar5486@gmail.com>
2023-04-20 00:27:38 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import './App.css';
 
 function App() {
   return (
+    <fragment>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Benifits of Using React..</h1>
+        <ol>
+          <li>
+            component based architecture
+          </li>
+          <li>
+            Virtual DOM for efficient update
+          </li>
+          <li>
+            Rich ecosystem and community
+          </li>
+          <li>
+            Cross platform development
+          </li>
+          <li>
+            Strong community support
+          </li>
+        </ol>
+        <button>
+          Get Started..
+        </button>
+
       </header>
+     
     </div>
+    
+    </fragment>
   );
 }
 
