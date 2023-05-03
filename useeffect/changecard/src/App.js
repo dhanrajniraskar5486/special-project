e4065888933cc131cab51e18dc9db88332b8ecfd
Dhanraj Niraskar <dhanrajniraskar5486@gmail.com>
2023-05-03 10:28:26 +0530

    Done

---
+++ useeffect/changecard/src/App.js
@@ -0,0 +1,12 @@
+import CardMaking from './component/changecard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <CardMaking/>
+    </div>
+  );
+}
+
+export default App;
